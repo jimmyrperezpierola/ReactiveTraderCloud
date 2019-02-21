cf7eae7270bfacef4aaab94d7ac00d7291a546f5
Keshal Desai <keshaldesai@gmail.com>
2019-02-20 19:26:12 -0500

    lazy load shell routes

--- src/client/src/shell/Router.tsx
+++ src/client/src/shell/Router.tsx
@@ -1,8 +1,13 @@
-import React, { SFC } from 'react'
+import React, { SFC, lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { PlatformAdapter, withPlatform } from 'rt-components'
-import { AnalyticsRoute, BlotterRoute, OpenFinRoute, ShellRoute, SpotRoute } from './routes'
+
+const OpenFinRoute = lazy(() => import('./routes/OpenFinRoute'))
+const ShellRoute = lazy(() => import('./routes/ShellRoute'))
+const AnalyticsRoute = lazy(() => import('./routes/AnalyticsRoute'))
+const BlotterRoute = lazy(() => import('./routes/BlotterRoute'))
+const SpotRoute = lazy(() => import('./routes/SpotRoute'))
 
 const ShellSwitchRoute = ({ header, platform }: { header: React.ReactChild; platform: PlatformAdapter }) =>
   platform.name === 'openfin' ? <OpenFinRoute /> : <ShellRoute header={header} />
@@ -11,11 +16,13 @@ const ShellSwitchRouteWithPlatform = withPlatform(ShellSwitchRoute)
 
 export const Router: SFC = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={ShellSwitchRouteWithPlatform} />
-      <Route path="/analytics" component={AnalyticsRoute} />
-      <Route path="/blotter" component={BlotterRoute} />
-      <Route path="/spot/:symbol" component={SpotRoute} />
-    </Switch>
+    <Suspense fallback={<div />}>
+      <Switch>
+        <Route exact path="/" component={ShellSwitchRouteWithPlatform} />
+        <Route path="/analytics" component={AnalyticsRoute} />
+        <Route path="/blotter" component={BlotterRoute} />
+        <Route path="/spot/:symbol" component={SpotRoute} />
+      </Switch>
+    </Suspense>
   </BrowserRouter>
 )
